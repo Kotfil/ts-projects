214d1e240444c78faf7d9e38211833fe3ef21be8
FIlipp <filippkotenko@outlook.com>
2020-07-15 23:26:38 +0300

    onClick

--- tester/src/components/Accordion.tsx
+++ tester/src/components/Accordion.tsx
@@ -1,29 +1,50 @@
 import React from "react";
 
-function Accordion(props: any) {
-    console.log('Accordion render')
+type AccordionPropsType = {
+    titleValue: string
+    harmonic: boolean
+}
+
+
+
+
+function Accordion(props: AccordionPropsType) {
+    console.log('Accordion render');
+    if (props.harmonic === true) {
+        return <div>
+        <AccordionTitle title={props.titleValue}/>
+        <AccordionBody/>
+        </div>
+    } else {
+        return <AccordionTitle title={props.titleValue}/>
+    }
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: any) {
+    console.log('AccordionTitle render');
+    return <div>
+        <h3>---{props.title}---</h3>
+    </div>
+}
+
+
+function AccordionBody(props: any) {
+    console.log('AccordionBody render');
     return (
         <div>
-            <AccordionTitle title={props.title}/>
-            <AccordionBody title={props.title}/>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
         </div>
     );
 }
 
-function AccordionTitle(props:any) {
-    console.log('AccordionTitle render')
-    return <div>
-        <h1>{props.title}</h1>
-    </div>
-}
-
-function AccordionBody(props: any) {
-    console.log('AccordionBody render')
-    return (
-    <div>
-        <h1>{props.title}</h1>
-    </div>
-    );
-}
 
 export default Accordion;
