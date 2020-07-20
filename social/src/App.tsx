5f9a8b517e47c102fafe361e29fd71935ab932ef
Kotfil <Dramen333>
2020-07-20 23:34:37 +0300

    refactor Dialogs

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -3,15 +3,19 @@ import './App.css';
 import Profile from "./components/Profile/Profile";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                <Dialogs/></div>
+            {/*<Profile/>*/}
         </div>
     );
 }
 
 export default App;
+
