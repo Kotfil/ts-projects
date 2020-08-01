db9117ec0f36a0277ea00d6e0691c7adba1f8e6e
FilippK <filippkotenko@outlook.com>
2020-08-01 16:30:45 +0300

    5 kata

--- codewars/src/index.js
+++ codewars/src/index.js
@@ -29,10 +29,32 @@ import './style.css'
 
 //console.log(repeatStr(3,'hello'));
 //5.
-function digitize(n) {
-     return n.toString().split('').reverse().map(x=>+x)
-}
-console.log(digitize(345678))
+// function digitize(n) {
+//      return n.toString().split('').reverse().map(x=>+x)
+// }
+// console.log(digitize(345678))
+//  var array = [];
+//     for (var i = 0; i <= 20; i++) {
+//         array.push(Math.random().toFixed(12))
+//     }
+//     console.log(array);
+// var array1 = [1,222,33,41,56,2];
+// var array = [];
+// for (var i = 0; i < array1.length; i++) {
+//     if(array1[i] > 0 && array1[i] < 10) {
+//         console.log(array1[i])
+//     }
+//     var arr = ['ss','WWW','fwf','ww','WWEDF'];
+// //    let ff = arr.filter(t =>  t.toUpperCase() === t)
+// // console.log(ff)
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//     for(var i = 0;i < arr.length;i++) {
+//         if (arr[i] == 5) {
+//              console.log(arr[i])
+//         }
+//     }
+
+
 
 function App() {
     return (
