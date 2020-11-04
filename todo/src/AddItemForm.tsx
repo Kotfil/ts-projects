1eca637ebe6ab0c59e277d1e03a890b58ca5c1f4
Fil <filippkotenko@outlook.com>
2020-11-04 20:57:41 +0200

    snapscreen storybook

--- todo/src/AddItemForm.tsx
+++ todo/src/AddItemForm.tsx
@@ -2,12 +2,12 @@ import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import {Box, Button, FormControl, IconButton, TextField} from '@material-ui/core';
 import {AddBox} from '@material-ui/icons';
 
-type AddItemFormPropsType = {
+export type AddItemFormPropsType = {
     addItem: (title: string) => void
 }
 
-export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
-    console.log('AddItemForm is called')
+export const AddItemForm = React.memo( (props: AddItemFormPropsType) => {
+    console.log("AddItemForm is called")
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
 
@@ -25,11 +25,11 @@ export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
     }
 
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if(error !== null) {
+        if (error !== null) {
             setError(null);
-            if (e.charCode === 13) {
-                addItem();
-            }
+        }
+        if (e.charCode === 13) {
+            addItem();
         }
     }
 
@@ -46,4 +46,4 @@ export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
             <AddBox />
         </IconButton>
     </div>
-})
+} );
